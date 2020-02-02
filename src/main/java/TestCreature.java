88420bf3431c6cfde9edd478828fe9e540b1e8db
Ubuntu <ubuntu@ip-172-31-4-245.us-east-2.compute.internal>
2020-02-02 17:55:53 +0000

    Finishing creating Tiger class and updated test cases.

--- src/main/java/TestCreature.java
+++ src/main/java/TestCreature.java
@@ -1,7 +1,7 @@
 public class TestCreature {
 
-    public static final int CREATURE_COUNT = 1;
-    public static final int THING_COUNT = 3;
+    public static final int CREATURE_COUNT = 3;
+    public static final int THING_COUNT = 6;
     
     public static void main(String[] args) {
 	Thing[] things = new Thing[THING_COUNT];
@@ -9,10 +9,17 @@ public class TestCreature {
 	Thing t0 = new Thing("machine");
 	Thing t1 = new Thing("animal");
 	Thing t2 = new Thing("human");
-
+	Tiger t3 = new Tiger("Tony");
+	Tiger t4 = new Tiger("Felix");
+	Tiger t5 = new Tiger("Leo");
+	
 	things[0] = t0;
 	things[1] = t1;
 	things[2] = t2;
+	things[3] = t3;
+	things[4] = t4;
+	things[5] = t5;
+	
 	for(Thing t: things) {
 	    System.out.println(t);
 	}
---
+++ src/main/java/Tiger.java
@@ -0,0 +1,11 @@
+public class Tiger extends Creature{
+
+    public Tiger(String name) {
+	super(name);
+    }
+
+    public void move() {
+	System.out.println(this.toString()  + " has just pounced.");
+    }
+    
+}
